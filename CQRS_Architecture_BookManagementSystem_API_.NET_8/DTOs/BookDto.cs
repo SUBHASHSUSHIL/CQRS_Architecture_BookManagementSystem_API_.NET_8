fe742a40f8eae1eb0974196e84bfc75fe06e1e6d
SUSHIL THAKUR <mt3028788@gmail.com>
2025-06-11 15:45:11 +0530

    add dto file

--- CQRS_Architecture_BookManagementSystem_API_.NET_8/DTOs/BookDto.cs
+++ CQRS_Architecture_BookManagementSystem_API_.NET_8/DTOs/BookDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CQRS_Architecture_BookManagementSystem_API_.NET_8.Models.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,11 @@ namespace CQRS_Architecture_BookManagementSystem_API_.NET_8.DTOs
 {
     public class BookDto
     {
-
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public byte[]? Cover { get; set; }
+        public Guid AuthorId { get; set; }
+        public Author Author { get; set; }
+        public int PublishYear { get; set; }
     }
 }
